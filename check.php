5fb535062b114e0d78de99da8cbc161eae3f1c51
Yoganarasimha12 <116260597+Yoganarasimha12@users.noreply.github.com>
2025-11-04 12:56:03 +0530

    Create check.php

---
+++ check.php
@@ -0,0 +1,6 @@
+console.log("emailModalData being sent to modal:", {
+  creditStatus: props.applicationInfo?.applicationStatus,
+  customerName: fullName,
+  card_type: props.applicationInfo?.card_type,
+  deliveryStatus: cardInfo?.deliveryStatus,
+});
