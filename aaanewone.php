7227ca9ffc533b36952414b01395d762e61aa42f
Yoganarasimha12 <116260597+Yoganarasimha12@users.noreply.github.com>
2025-11-04 22:11:03 +0530

    Update aaanewone.php

--- aaanewone.php
+++ aaanewone.php
@@ -41,3 +41,70 @@
     </div>
   </div>
 )}
+
+
+(isApproved && (
+
+FinalApproval
+
+<div className="approved-info-block mt-2">
+
+<div className="d-flex justify-content-between py-2">
+
+<div className="premium-info-row">
+
+<span className="label">Issued Date:</span>
+
+<span className="value">
+
+(cardInfo?.generatedAt ? cardInfo.generatedAt.slice(0, 10): "-")
+
+</span>
+
+</div>
+
+</div>
+
+<div className="d-flex justify-content-between py-2">
+
+<div className="premium-info-row">
+
+<span className="label">Status:</span>
+
+<span </div>
+
+className="value">{cardInfo?.cardStatus || "-"}</span>
+
+</div>
+
+</div>
+
+03323
+
+className="premium-card-visual ms-3"
+
+style={{
+
+flex: "320px",
+
+minHeight: 200,
+
+display: "flex",
+
+justifyContent: "center",
+
+alignItems: "center",
+
+(isApproved && (
+
+<div className="credit-card-ui">
+
+<div className="bank-logo">
+
+<img src="/Asset/Logos/try_scb_logo.png" alt="Standard Chartered" />
+
+</div>
+
+<div className="chip" />
+
+<div className="cc-number">
